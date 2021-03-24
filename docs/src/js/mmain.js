2441542622a717cb72e121debd426bd42937cffd
Yuri Morozov <moroz69off@users.noreply.github.com>
2021-03-24 22:53:40 +0300

    Update mmain.js

--- docs/src/js/mmain.js
+++ docs/src/js/mmain.js
@@ -1,4 +1,4 @@
-import * as THREE from './three/three.module.js';
+import * as THREE from '../three/three.module.js';
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("rgb(22, 222, 222)");
 console.log(scene.background);
