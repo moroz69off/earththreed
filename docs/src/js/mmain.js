09682ba7cb68da72f56fd026d5fc41f4911f7788
Морозов Юрий <moroz69off@gmail.com>
2021-03-24 22:37:16 +0300

    add local

--- docs/src/js/mmain.js
+++ docs/src/js/mmain.js
@@ -1,1 +1,4 @@
-console.log('<a mmain.js>');
+import * as THREE from 'https://moroz69off.github.io/earththreed/three/three.module.js';
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("rgb(22, 222, 222)");
+console.log(scene.background);
