89ad0046b9552930dcd6862aa701ab52a4b52376
Yuri Morozov <moroz69off@users.noreply.github.com>
2021-03-24 23:01:27 +0300

    Update mmain.js

--- docs/src/js/mmain.js
+++ docs/src/js/mmain.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://moroz69off.github.io/earththreed/three/three.module.js';
 
-const scene = new THREE.Scene(){
-  background : new THREE.Color("rgb(22, 222, 222)")
-};
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("rgb(22, 222, 222)");
+
 console.log(scene.background);
