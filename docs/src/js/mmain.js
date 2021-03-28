a8582b765de90ae6cee0fa33446d477690c1e315
Морозов Юрий <moroz69off@gmail.com>
2021-03-28 19:01:40 +0300

    repair all
    Signed-off-by: Морозов Юрий <moroz69off@gmail.com>

--- docs/src/js/mmain.js
+++ docs/src/js/mmain.js
@@ -1,17 +1,10 @@
-//import * as THREE from 'https://moroz69off.github.io/earththreed/three/three.module.js';
-
-var /*const*/ scene = null;
-var /*const*/ camera = null;
-var /*const*/ renderer = null;
-var /*const*/ earth_radius = 25;
-var /*const*/ latitude_segments = 32;  //широта
-var /*const*/ longitude_segments = 32; //долгота
+var obj_body = null;
+var controls = null;
+var earth_radius = 25;
+var latitude_segments = 32;
+var longitude_segments = 32;
 var /*const*/ loading_manager = new THREE.LoadingManager();
 var /*const*/ texsture_loader = new THREE.TextureLoader(loading_manager);
-var /*const*/ camera_orbit_control = null;
-var /*const*/ wrf = null;
-
-var WR = THREEx;
 
 loading_manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
 	console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
@@ -27,65 +20,60 @@ loading_manager.onError = function ( url ) {
 
 loading_manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
 	console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+	var percentComplete = itemsLoaded / itemsTotal * 100;
+	console.log(Math.round(percentComplete, 2) + '% downloaded');
 };
 
-const earth_map = texsture_loader.load('./src/texture/earth_map_texture.gif');
+var /*const*/ earth_texture = texsture_loader.load('./src/texture/earth_map_texture.gif');
 
 ThreeInit();
 
 function ThreeInit () {
 	window.onload = function () {
-		scene = new THREE.Scene();
-		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-		renderer = new THREE.WebGLRenderer({
-			antialias: true,
-			alpha: true,
-			canvas: document.getElementById('c')
-		});
+		var /*const*/ scene = new THREE.Scene();
+		var /*const*/ camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+		var /*const*/ renderer = new THREE.WebGLRenderer({ antialias: true });
+
 		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.setClearColor(0x000000);//0xCFCFCF or 0x000000
+		renderer.setClearColor(0x000000); // 0xCFCFCF
+
+		document.body.appendChild(renderer.domElement);
+
+		camera.position.z = 50;
+
+		THREEx.WindowResize(renderer, camera);
 		
-		const earth_geometry = new THREE.SphereGeometry(
+		var /*const*/ cam_orbit_control = new THREE.OrbitControls(camera);
+		cam_orbit_control = new THREE.OrbitControls(camera);
+		cam_orbit_control.autoRotate = false;
+		//cam_orbit_control.autoRotateSpeed = 1.111;
+		cam_orbit_control.enablePan = false;
+		cam_orbit_control.maxPolarAngle = 2.34;
+		cam_orbit_control.minPolarAngle = .567;
+		cam_orbit_control.enableDamping = true;
+
+		var /*const*/ earth_geometry = new THREE.SphereGeometry(
 			earth_radius,
 			latitude_segments,
 			longitude_segments
 		);
-		const earth_material = new THREE.MeshBasicMaterial({
-			color: 0xFFFFFF,
-			map: earth_map
-		});
-		const earth = new THREE.Mesh( earth_geometry, earth_material );
-		camera.rotation.z = 0.4091; // angle of inclination of the earth's axis (23.44° to radian)
+		var /*const*/ earth_material = new THREE.MeshBasicMaterial({
+				color: 0xFFFFFF,
+				map: earth_texture
+			}
+		);
+		var /*const*/ earth = new THREE.Mesh( earth_geometry, earth_material );
+
 		scene.add( earth );
 
-		camera.position.z = 50;
+		var /*const*/ render = function () {
+			requestAnimationFrame(render);
+			earth.rotation.y += .005;
+			cam_orbit_control.update();
+			//camera.rotation.z = 0.4091; // angle of inclination of the earth's axis (23.44° to radian)
+			renderer.render(scene, camera);
+		};
 
-		wrf = new WR.WindowResize(renderer, camera);
-
-		camera_orbit_control = new THREE.OrbitControls(camera);
-		camera_orbit_control.autoRotate = true;
-		camera_orbit_control.enablePan = false;
-		camera_orbit_control.maxPolarAngle = 2.34;
-		camera_orbit_control.minPolarAngle = .567;
-
-		//camera_orbit_control.enableDamping = true;
-
-		let canvasus = document.getElementById('c');
-		canvasus.onclick = function(event) {
-			console.log(event);
-		}
-
-
-
-		//console.log(camera_orbit_control);
-
-		function animate() {
-			requestAnimationFrame( animate );
-			//===ANIMATION HERE=====
-			camera_orbit_control.update();
-			//====END ANIMATION=====
-			renderer.render( scene, camera );
-		}
-		animate();
+		render();
 	}
 }
